90f970d51115ae7b2c5b66ba85d70b58b8864119
Maximilian Mackh <mmackh@gmail.com>
2020-12-08 05:17:42 +0100

    open variable

--- Swifty360Player/Swifty360View.swift
+++ Swifty360Player/Swifty360View.swift
@@ -91,7 +91,7 @@ open class Swifty360View: UIView {
     private var underlyingSceneSize: CGSize!
     private var sceneView: SCNView!
     private var playerScene: Swifty360PlayerScene!
-    private var cameraController: Swifty360CameraController!
+    open var cameraController: Swifty360CameraController!
 
     public init(withFrame frame: CGRect,
                 player: AVPlayer,
